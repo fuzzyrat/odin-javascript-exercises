76fbccd0ad4ad76e2a41e00868ac65a593368e9f
Mihael <miki_bez@hotmail.com>
2025-06-20 14:39:36 +0800

    implemented and tested 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newValue = (temp - 32) * (5/9);
+  if (newValue % 1 == 0) return Math.round(newValue);
+  else return Number(newValue.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newValue = (temp * (9/5) + 32);
+  if (newValue % 1 == 0) return Math.round(newValue);
+  else return Number(newValue.toFixed(1));
+  ;
 };
 
 // Do not edit below this line
