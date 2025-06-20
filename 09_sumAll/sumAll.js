1798700c4f90aec27cf0830eb3e03cf35d0a7151
Mihael <miki_bez@hotmail.com>
2025-06-20 14:04:29 +0800

    implemented and tested 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 > num2) {
+        const temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    
 
+    let currentNumber = num1;
+    let finalSum = 0;
+    for (i = num1; i <= num2; i++) {
+        finalSum += currentNumber;
+        currentNumber += 1;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
